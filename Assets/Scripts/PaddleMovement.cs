cfa9b3719438d3073d5c9bade9966a83b1eda941
Anabela Marto <anabellamarto@gmail.com>
2023-05-06 17:41:51 +0100

    Add colliders applying  forces to the paddles.

--- Assets/Scripts/PaddleMovement.cs
+++ Assets/Scripts/PaddleMovement.cs
@@ -8,18 +8,23 @@ public class PaddleMovement : MonoBehaviour
     private float moveSpeed = 1.0f; //não fica visível no inspetor e tem um valor default de 1 (o ponto zero e o f indica que é um float)
     
     [SerializeField]
-    private string playerControls = "Vertical1P";
+    private string axisName = "Vertical1P";
+
+    private Rigidbody2D paddleRB;
+    
 
     // Start is called before the first frame update
     private void Start()
     {
-        
+        paddleRB = this.GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
     private void Update()
     {
-        float verticalMovement = Input.GetAxis(playerControls);
-        transform.Translate(new Vector2(0, verticalMovement) * moveSpeed * Time.deltaTime);
+        float verticalMovement = Input.GetAxis(axisName); 
+        Vector2 moveForce = new Vector2(0, verticalMovement) * moveSpeed * Time.deltaTime;
+        
+        paddleRB.AddForce(moveForce);
     }
 }
